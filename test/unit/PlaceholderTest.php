998d73497d4ee0bc11e7db4b7ce38100e0172e10
jdoubleyou <jwilliamson@sourceallies.com>
2022-07-27 11:12:59 -0500

    Add files via upload

---
+++ test/unit/PlaceholderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace test\unit;
+
+use PHPUnit\Framework\TestCase;
+
+
+class PlaceholderTest extends TestCase {
+    public function testThatThisConditionIsTrue() {
+        $this->assertTrue(true);
+    }
+}
+
+?>
